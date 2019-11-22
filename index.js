13da412ef6be12c3eef8a39e5c5c3acf207495fa
hs-yonekura <y-yonekura@science.co.jp>
2019-11-22 13:22:53 +0900

    all fixed

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ const cheerio = require('cheerio');
 module.exports = {
   hooks: {
     'page': function(page) {
-      console.log(JSON.stringify(page));
+      const $ = cheerio.load(page.content);
+      $('table').each(function(i, table){
+        if (/^{[\s\d\|(px)(em)(pt)%]+}$/g.test($(table).prev().text()))
+        {
+          var widths = $(table).prev().text().match(/[\d(px)(em)(pt)%]+/g);
+          $(table).find('th').each(function(i, th){
+            if (i < widths.length)
+              $(th).attr('width', widths[i]);
+          });
+          $(table).prev().remove();
+        }
+      });
+      page.content = $.html();
+      return page;
     }
   }
+
 }
